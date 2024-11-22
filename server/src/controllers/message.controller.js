4fdb783f2aa700461adaaf8ca51559a8a05dcaa8
prabalbhandary <prabalbhandary97@gmail.com>
2024-11-22 19:45:40 +0545

    updates

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -9,10 +9,10 @@ export const getUsersForSidebar = async (req, res) => {
     const loggedInUserId = req.user._id;
     const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
-    res.status(200).json(filteredUsers);
+    res.status(200).json({success: true, filteredUsers, message: "Users fetched successfully"});
   } catch (error) {
     console.error("Error in getUsersForSidebar: ", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({success: false, error: "Internal server error" });
   }
 };
 
@@ -28,7 +28,7 @@ export const getMessages = async (req, res) => {
       ],
     });
 
-    res.status(200).json(messages);
+    res.status(200).json({success: true, message: "Messages fetched successfully", messages});
   } catch (error) {
     console.log("Error in getMessages controller: ", error.message);
     res.status(500).json({ error: "Internal server error" });
@@ -43,7 +43,6 @@ export const sendMessage = async (req, res) => {
 
     let imageUrl;
     if (image) {
-      // Upload base64 image to cloudinary
       const uploadResponse = await cloudinary.uploader.upload(image);
       imageUrl = uploadResponse.secure_url;
     }
@@ -65,6 +64,6 @@ export const sendMessage = async (req, res) => {
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller: ", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ success: false, error: "Internal server error" });
   }
 };
